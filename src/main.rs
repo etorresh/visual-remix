9e496bf92170a94a3aad20bb903725d046a72e0e
etorresh <me@etorresh.com>
2022-11-28 19:40:22 -0700

    added ndarray

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
-    println!("Hello, world!");
-}
+use video_rs::{
+    self,
+    Locator,
+    Decoder,
+  };
+  
+  fn main() {
+    video_rs::init();
+    
+    let source = Locator::Url("http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4".parse().unwrap());
+    let mut decoder = Decoder::new(&source)
+      .expect("failed to create decoder");
+  
+    for frame in decoder.decode_iter() {
+      if let Ok((_, frame)) = frame {
+        let rgb = frame
+          .slice(ndarray::s![0, 0, ..])
+          .to_slice()
+          .unwrap();
+        println!(
+          "pixel at 0, 0: {}, {}, {}",
+          rgb[0], rgb[1], rgb[2],
+        );
+      } else {
+        break;
+      }
+    }
+  
+  }

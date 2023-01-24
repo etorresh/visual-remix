f84c667537af33c10c32e8925122f1174be3a035
etorresh <me@etorresh.com>
2023-01-24 15:04:55 -0700

    testing gstreamer and png structure

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,29 @@
+// http://www.libpng.org/pub/png/spec/1.2/PNG-Structure.html
+
 use std::fs::File;
 use std::io::Write;
 use std::path::Path;
 
 // Capture fist frame of the video file
 fn main() {
-    let file_path = Path::new("resources/video.mp4");
-    let mut first_frame = File::create("resources/first_frame.png").unwrap();
+    // Green screen rgb: (114, 251, 2)
+    let file_path = Path::new("resources/greenscreen.mp4");
+    // let mut first_frame = File::create("resources/first_frame.png").unwrap();
 
-    let frame_source = vid2img::FileSource::new(file_path, (500, 1000)).unwrap();
+    let frame_source = vid2img::FileSource::new(file_path, (500, 500)).unwrap();
+    let mut x = 0;
     for frame in frame_source.into_iter() {
         if let Ok(Some(png_img_data)) = frame {
-            println!("{}", png_img_data.len());
-            first_frame.write_all(&png_img_data).unwrap();
-            break;
+            if x == 1 {
+                let mut y = 0;
+                for pixel in png_img_data {
+                    println!("{}", pixel);
+                    y += 1;
+                    if y == 10 {break};
+                }
+                break
+            }
+            x += 1;
         }
     }
 }
